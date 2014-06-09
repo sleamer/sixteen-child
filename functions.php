a95d3907a452e0a0f60960bb4c0beb763bddf378
Seth Leamer <sleamer@hotmail.com>
2014-06-09 14:17:19 -0700

    this is new fixes, need to move content-single.php to the child folder

--- functions.php
+++ functions.php
@@ -5,14 +5,64 @@ function my_fu_after_upload( $media_ids, $success ) {
 	// Something went wrong with upload
 	if ( ! $success )
 		return;
+$media = get_attached_media( 'image', $_POST['post_ID'] ); 
+// print_r ($media);
+// $id = 
+reset($media);
+$first_key = key($media);
+// echo $first_key;
+$imgID = $media[$first_key]->guid;
+set_post_thumbnail( $_POST['post_ID'], $imgID );
+// Update post 37
+  $my_post = array(
+      'ID'           => $_POST['post_ID'],
+      'post_content' => 'This is the updated content.'
+  );
 
-	// There might be multiple attachments so no really good way to assign image
-	foreach ( (array) $media_ids as $thumbnail_id ) {
-		// Set the featured image and
-		// Stop trying to set it after first successful attempt
-		if ( set_post_thumbnail( $_POST['post_ID'], $thumbnail_id ) )
-			break;
-	}
+// Update the post into the database
+  wp_update_post( $my_post );
+
 }
 
+
 ?>
+
+<?php
+/*
+add_filter("manage_upload_columns", 'upload_columns');
+add_action("manage_media_custom_column", 'media_custom_columns', 0, 2);
+
+function upload_columns($columns) {
+
+	unset($columns['parent']);
+	$columns['better_parent'] = "Parent";
+
+	return $columns;
+
+}
+ function media_custom_columns($column_name, $id) {
+
+	$post = get_post($id);
+
+	if($column_name != 'better_parent')
+		return;
+
+		if ( $post->post_parent > 0 ) {
+			if ( get_post($post->post_parent) ) {
+				$title =_draft_or_post_title($post->post_parent);
+			}
+			?>
+			<strong><a href="<?php echo get_edit_post_link( $post->post_parent ); ?>"><?php echo $title ?></a></strong>, <?php echo get_the_time(__('Y/m/d')); ?>
+			<br />
+			<a class="hide-if-no-js" onclick="findPosts.open('media[]','<?php echo $post->ID ?>');return false;" href="#the-list"><?php _e('Re-Attach'); ?></a></td>
+
+			<?php
+		} else {
+			?>
+			<?php _e('(Unattached)'); ?><br />
+			<a class="hide-if-no-js" onclick="findPosts.open('media[]','<?php echo $post->ID ?>');return false;" href="#the-list"><?php _e('Attach'); ?></a>
+			<?php
+		}
+
+}
+*/?>
